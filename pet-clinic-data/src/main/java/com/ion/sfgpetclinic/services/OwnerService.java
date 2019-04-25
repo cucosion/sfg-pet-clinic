8b89897c80589f2dd48b3bf229394753217431e6
Ion (I.) Cucos <ion.cucos2@ing.com>
2019-04-25 12:24:28 +0200

    Added service interface. Closes #5.

---
+++ pet-clinic-data/src/main/java/com/ion/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,12 @@
+package com.ion.sfgpetclinic.services;
+
+import com.ion.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+    Owner findByLastName(String lastName);
+    Owner findById(Long id);
+    Owner save(Owner owner);
+    Set<Owner> findAll();
+}
